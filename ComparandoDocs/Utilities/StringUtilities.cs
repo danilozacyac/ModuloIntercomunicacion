cbc1ba5fbc0ddb60cf2b7c80243f78f841d459af
Danilo <danilozacyac@gmail.com>
2014-03-28 14:55:54 -0600

    Avance en compraracion

---
+++ ComparandoDocs/Utilities/StringUtilities.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ComparandoDocs.Utilities
+{
+    public class StringUtilities
+    {
+
+        public static char ContainPunctuation(String texto)
+        {
+            char[] puntuacion = { ',','.','\"','-',';',':' };
+
+            foreach (char charac in puntuacion)
+            {
+                if (texto.Contains(charac))
+                    return charac;
+            }
+
+            return ' ';
+        }
+
+    }
+}
